6f606cd8d1e4b759189fc6dda8b39f22bbc3332c
MrIochy <157193100+MrIochy@users.noreply.github.com>
2025-05-16 22:40:35 -0600

    ejercicio b Completado

--- Practica1/ejercicio b.js
+++ Practica1/ejercicio b.js
@@ -7,3 +7,10 @@ function cuadrado(numero) {
 //console.log();
 //muestra en consola en probando con 3 numeros diferentes
 
+// Arrow function para calcular el cuadrado
+const cuadrado = numero => numero * numero;
+
+// Probando con 3 números diferentes
+console.log(cuadrado(2));    // El resultado deberia ser 4
+console.log(cuadrado(5));    // El resultado deberia ser 25
+console.log(cuadrado(10));   // El resultado deberia ser 100
