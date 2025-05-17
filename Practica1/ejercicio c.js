d51929214c69d6a1bb27a6e499b8cd262818444b
MrIochy <157193100+MrIochy@users.noreply.github.com>
2025-05-16 22:46:24 -0600

    ejercicio c Completado (Practica 1 Completa)

--- Practica1/ejercicio c.js
+++ Practica1/ejercicio c.js
@@ -2,3 +2,9 @@
 parametros: nombre y edad, y retorne una cadena como la siguiente */
 
 // "Hola, me llamo Isay y tengo 37 años"
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años`;
+
+// Ejemplo de uso:
+console.log(saludoPersonalizado("Isay", 37));  // "Hola, me llamo Isay y tengo 37 años"
+console.log(saludoPersonalizado("Luis", 22));   // "Hola, me llamo Luis y tengo 22 años"
+console.log(saludoPersonalizado("Valeria", 22)); // "Hola, me llamo Valeria y tengo 22 años"
