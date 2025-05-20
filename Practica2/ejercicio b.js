e1f529507700c1de176f4a9126c4bf501e32a6af
MrIochy <157193100+MrIochy@users.noreply.github.com>
2025-05-20 11:15:34 -0600

    ejercicio b Completado

---
+++ Practica2/ejercicio b.js
@@ -0,0 +1,13 @@
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 }
+];
+
+// Tu codigo aqui
+const nombresProductosCaros = productos
+    .filter(producto => producto.precio > 1000)
+    .map(producto => producto.nombre);
+
+console.log(nombresProductosCaros); // ["Laptop", "Monitor"]
