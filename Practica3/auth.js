fe89f83917c5a34f691be6d24b1d0b248faa5c2c
MrIochy <157193100+MrIochy@users.noreply.github.com>
2025-05-25 23:29:55 -0600

    ejercicio b Completado

---
+++ Practica3/auth.js
@@ -0,0 +1,9 @@
+export function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
--- Practica3/main.js
+++ Practica3/main.js
@@ -1,5 +1,16 @@
 import { restar } from './utils.js';
+import { verificarUsuario } from './auth.js';
 
+// ejercicio a
 console.log(restar(10, 5));
 console.log(restar(3, 7));
 console.log(restar(0, 0));
+
+// ejercicio b
+verificarUsuario("admin")
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+verificarUsuario("Luis")
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
